48c07ac670b27474e6fff1e8048675896a94ada7
Guilherme Oliveira <guilherme.mth.oliveira@gmail.com>
2026-09-15 10:27:01 -0300

    finalizado formulario  para criação de eventos

--- app/Http/Controllers/EventController.php
+++ app/Http/Controllers/EventController.php
@@ -12,13 +12,24 @@ class EventController extends Controller
     $events = Event::all();
 
     return view('welcome',['events' => $events]);
-
-
 }
 
     public function create(){
         return view('events.create');
 
     }
+//função para trazer todos os dados que serão enviados pelo formulário de criar eventos
+    public function store(Request $request){
 
+        $event = new Event; // cria um novo registro vazio do model Event
+
+        $event->title = $request->title; // pega o campo "title" do formulário
+        $event->city = $request->city;
+        $event->private = $request->private;
+        $event->description = $request->description;
+
+        $event->save(); // salva tudo isso no banco de dados
+
+        return redirect('/'); //redireciona o usuário para a view home
+    }
 }
--- resources/views/events/create.blade.php
+++ resources/views/events/create.blade.php
@@ -4,6 +4,32 @@
 
 @section('content')
 
-<h1>Criar um Evento</h1>
+<div id="event-create-container" class="col-md-6 offset-md-3">
+    <h1>Crie o seu evento</h1>
+    <form action="/events" method="POST">
+        <div class="form-group">
+            <label for="title">Evento:</label>
+            <input type="text" class="form-control" id="title" name="title" placeholder="Nome do evento" required>
+        </div>
+        <div class="form-group">
+            <label for="title">Cidade:</label>
+            <input type="text" class="form-control" id="city" name="city" placeholder="Local do evento" required>
+        </div>
+        <div class="form-group">
+            <label for="title">O evento é privado?</label>
+            <select name="private" id="private" class="form-control" required>
+                <option value="" selected>Selecione...</option>
+                <option value="0">Não</option>
+                <option value="1">Sim</option>
+            </select>
+        </div>
+        <div class="form-group">
+            <label for="title">Descrição:</label>
+            <textarea name="description" id="description" class="form-control" placeholder="O que vai acontecer no evento" required></textarea>
+        </div>
+        <input type="submit" class="btn btn-outline-primary w-100 m-auto mt-5 btn-lg fw-bold fs-6   " value="Criar evento">
+    </form>
+
+</div>
 
 @endsection
