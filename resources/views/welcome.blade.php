632867a11198ddf4369db0bf3983d1616b863ad1
Guilherme Oliveira <guilherme.mth.oliveira@gmail.com>
2026-09-17 11:29:58 -0300

    finalização upload de imagens

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -17,7 +17,7 @@
         <div id="cars-container" class="row">
             @foreach ($events as $event)
                 <div class="card col-md-3">
-                    <img src="/img/card.jpg" alt="{{ $event->title }}">
+                    <img src="/img/events/{{ $event->image }}" alt="{{ $event->title }}">
                     <div class="card-body">
                         <p class="card-date">14/09/2026</p>
                         <h5 class="card-title">{{$event->title}}</h5>
